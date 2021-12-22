ac77620ccea5b3c5cb6e15889063a24153e14b57
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2021-12-22 08:21:26 -0500

    Automatic vs. allocatable arrays with error handling

---
+++ automatic.f90
@@ -0,0 +1,14 @@
+! If allocation errors must be handled, use allocatable rather than automatic arrays in procedures, 
+!  and check that stat of allocate.
+subroutine foo(x)
+implicit none
+real, intent(in)  :: x(:)
+real              :: y(size(x)) ! automatic array -- program crashes if there is no memory for it
+real, allocatable :: z(:)
+integer           :: ierr
+allocate (z(size(x)),stat=ierr)
+if (ierr /= 0) then ! allocate errors can be handled
+   print*,"could not allocate z in foo"
+   return
+end if
+end subroutine foo
