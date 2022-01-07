546f89f981612af239a491354f8131f201fbe99d
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-01-07 16:12:43 -0600

    Two forms of function syntax

---
+++ function.f90
@@ -0,0 +1,29 @@
+module m
+implicit none
+contains
+pure integer function twice(i)
+integer, intent(in) :: i
+twice = 2*i ! result has same name as function
+end function twice
+!
+pure function double(i) result(j)
+integer, intent(in) :: i
+integer             :: j
+j = 2*i ! result has different name from function
+end function double
+!
+! to return an array, you must define a separate result variable
+pure function double_vec(i) result(j)
+integer, intent(in) :: i(:)
+integer             :: j(size(i))
+j = 2*i
+end function double_vec
+!
+end module m
+!
+program test_func
+use m, only: twice, double, double_vec
+implicit none
+print*,twice(3),double(3) ! 6 6 
+print*,double_vec([3,4]) ! 6 8
+end program test_func
