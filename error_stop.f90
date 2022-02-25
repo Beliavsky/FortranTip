f4b8918fae8a14d56a0fea4696855cf9867c293f
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-02-25 06:05:14 -0600

    Demonstrate ERROR STOP

---
+++ error_stop.f90
@@ -0,0 +1,20 @@
+module m
+implicit none
+contains
+pure function sqrt_int(i) result(y)
+integer, intent(in) :: i
+real                :: y
+if (i < 0) error stop "need i >= 0 in sqrt_int"
+! plain stop not allowed in pure function
+y = sqrt(real(i))
+end function sqrt_int
+end module m
+!
+program main
+use m, only: sqrt_int
+implicit none
+print*,sqrt_int(-1)
+stop "normal termination"
+end program main
+! output:
+! need i >= 0 in sqrt_int
