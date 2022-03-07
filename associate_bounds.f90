f4176f2f482d41f9c8d9fc30c820c0cda36f5c2c
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-03-07 07:35:49 -0500

    Bounds of associate-name for whole array or array section selector

--- associate_bounds.f90
+++ associate_bounds.f90
@@ -3,6 +3,7 @@ implicit none
 real :: m(-2:3,0:9)
 associate (a => m, &    ! whole array
            b => m(:,:)) ! array section
-   print*,lbound(a), lbound(b) ! -2 0 1 1
+   print*,lbound(a) ! -2 0 -- same as m
+   print*,lbound(b) !  1 1
 end associate
 end program associate_bounds
