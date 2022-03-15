d016652d0784aad309ee60653765b136f83f1a85
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-03-15 08:52:07 -0500

    Add files via upload

---
+++ open_file.f90
@@ -0,0 +1,43 @@
+program open_file
+implicit none
+character (len=*), parameter :: fname = "temp.txt"
+integer, parameter :: iu = 10
+integer :: ierr
+open (unit=iu,file=fname,action="write")
+! compilers will typically overwrite temp.txt if it exists, 
+! but this is not mandated witH unspecified STATUS
+write (iu,*) "a"
+close (iu)
+open (unit=iu,file=fname,action="write",status="replace")
+write (iu,*) "b"
+close (iu)
+open (unit=iu,file=fname,action="write",status="new",iostat=ierr)
+if (ierr == 0) then
+   write (iu,*) "c"
+   close (iu)
+else
+   write (*,*) "file " // fname // " already connected"
+end if
+open (unit=iu,file=fname,action="write",status="new")
+write (iu,*) "c"
+end program open_file
+! gfortran output (ifort, g95, flang similar):
+!  file temp.txt already connected
+! At line 21 of file open_file.f90 (unit = 10)
+! Fortran runtime error: Cannot open file 'temp.txt': File exists
+
+
+
+
+
+
+
+
+
+
+! NEW the file must not exist; it will be created
+! UNKNOWN the file has a processor-dependent status (default)
+! REPLACE if the file does not exist, it is file is created and given
+! a status of OLD; if the file does exist, it is deleted, a
+! new file is created with the same name, and the file
+! is given a status of OLD
