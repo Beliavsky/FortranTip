50ddf104153181f72848be0c13864ed543cde7e8
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-03-18 07:11:33 -0500

    Write and read a direct access file

--- direct_access.f90
+++ direct_access.f90
@@ -5,20 +5,20 @@ integer, parameter :: nrec = 10**6,  & ! # of records
                       iu = 10          ! unit number
 integer            :: irec,iol
 real               :: xmat(nrec,nreals),xlast(nreals)
-character (len=*), parameter :: unformatted_file = "temp.bin", &
+character (len=*), parameter :: unfmt_file = "temp.bin", &
    fmt_g = "(*(g0,:,1x))"
 call random_number(xmat) ! each row of xmat will be a record
 inquire (iolength=iol) xmat(1,:) ! store record length in iol
 print fmt_g,"record length iol =",iol
 ! record length must be specified for direct access
-open (unit=iu,file=unformatted_file,access="direct", &
-  recl=iol,action="write") 
+open (unit=iu,file=unfmt_file,access="direct",recl=iol, &
+      action="write") 
 do irec=nrec,1,-1 ! records can written in any order
    write (iu,rec=irec) xmat(irec,:) ! record number irec
 end do
 close (iu)
-open (unit=iu,file=unformatted_file,access="direct", &
-      recl=iol,form="unformatted",action="read")
+open (unit=iu,file=unfmt_file,access="direct",recl=iol, &
+      form="unformatted",action="read")
 ! read the last record without looping over previous records
 read (iu,rec=nrec) xlast 
 print fmt_g,xmat(nrec,:),xlast ! verify it was correctly
