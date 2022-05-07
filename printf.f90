83247e972929f049b10de2ce4ebb19e14cfb6657
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-05-07 06:26:42 -0500

    Call C printf() from Fortran

---
+++ printf.f90
@@ -0,0 +1,25 @@
+program call_printf ! GitHub FortranTip printf.f90
+use iso_c_binding, only: c_int, c_char, c_null_char, c_new_line
+interface
+function print_string(fmt,s) result(ierr) bind(c,name="printf")
+! Use C printf() to print string with format fmt
+import c_char
+character(c_char) :: fmt(*), s(*)
+end function print_string
+end interface
+interface
+function print_integer(fmt,i) result(ierr) bind(c,name="printf")
+! Use C printf() to print integer with format fmt
+import c_char, c_int
+character(c_char) :: fmt(*)
+integer(kind=c_int), intent(in), value :: i
+end function print_integer
+end interface
+integer :: ierr
+ierr = print_string("string is %s" // c_new_line // c_null_char, &
+                    "abc" // c_null_char)
+ierr = print_integer("int is %d" // c_new_line // c_null_char, 42_c_int)
+end program call_printf
+! output:
+! string is abc
+! int is 42
