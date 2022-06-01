43af82c47e66ec4e77e7b7fdcb3492cc5a589b00
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-06-01 09:34:02 -0400

    use sequence association to pass 1-D array to 2-D array argument

---
+++ print_mat.f90
@@ -0,0 +1,27 @@
+module m
+implicit none
+contains
+subroutine print_matrix(n1,n2,mat)
+integer, intent(in) :: n1, n2, mat(n1,n2)
+integer             :: i1
+do i1=1,n1
+   print "(*(1x,i4))", mat(i1,:)
+end do
+print*
+end subroutine print_matrix
+end module m
+!
+program main
+use m, only: print_matrix
+integer, parameter :: n = 6
+integer            :: v(n)
+v = [10,20,30,11,21,31]
+call print_matrix(2,3,v) ! coerce v to 2x3 matrix
+call print_matrix(3,2,v) ! coerce v to 3x2 matrix
+call print_matrix(2,2,v(3)) 
+! line below is clearer than line above
+call print_matrix(2,2,v(3:n)) 
+! If the actual argument has more elements than
+! the dummy argument, the excess elements are ignored.
+call print_matrix(2,2,v)
+end program main
