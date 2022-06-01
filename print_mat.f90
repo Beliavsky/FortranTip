275cb873c29f4c1f70be068c0bd288e0516fd151
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2022-06-01 12:39:38 -0400

    use sequence association to pass 1-D array to 2-D array argument

--- print_mat.f90
+++ print_mat.f90
@@ -2,7 +2,8 @@ module m ! GitHub FortranTip print_mat.f90
 implicit none
 contains
 subroutine print_matrix(n1,n2,mat)
-integer, intent(in) :: n1, n2, mat(n1,n2)
+integer, intent(in) :: n1, n2
+integer, intent(in) :: mat(n1,n2)
 integer             :: i1
 do i1=1,n1
    print "(*(1x,i4))", mat(i1,:)
@@ -16,12 +17,28 @@ use m, only: print_matrix
 integer, parameter :: n = 6
 integer            :: v(n)
 v = [10,20,30,11,21,31]
-call print_matrix(2,3,v) ! coerce v to 2x3 matrix
-call print_matrix(3,2,v) ! coerce v to 3x2 matrix
-call print_matrix(2,2,v(3)) 
+call print_matrix(2,3,v) ! pass v as 2x3 matrix
+call print_matrix(3,2,v) ! pass v as 3x2 matrix
+call print_matrix(2,2,v(3)) ! pass v(3:6) as 2x2 matrix
 ! line below is clearer than line above
 call print_matrix(2,2,v(3:n)) 
 ! If the actual argument has more elements than
 ! the dummy argument, the excess elements are ignored.
-call print_matrix(2,2,v)
+call print_matrix(2,2,v) ! pass v(1:4) as 2x2 matrix
 end program main
+! output:
+!    10   30   21
+!    20   11   31
+! 
+!    10   11
+!    20   21
+!    30   31
+! 
+!    30   21
+!    11   31
+! 
+!    30   21
+!    11   31
+! 
+!    10   30
+!    20   11
