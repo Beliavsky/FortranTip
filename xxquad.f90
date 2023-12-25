20db4e146f8d68c67a3463cb2e31f54f8e11dae3
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2023-12-25 07:23:31 -0500

    declare and set quadruple precision variables in a standard-conforming way

---
+++ xxquad.f90
@@ -0,0 +1,9 @@
+use iso_fortran_env, only: real128
+implicit none
+integer, parameter :: qp = real128 
+! integer, parameter :: qp = selected_real_kind(33, 4931) ! alternative
+real(kind=real128), parameter :: x = 1.1e4932_qp
+! line below is non-standard because of real*16 and Q
+real*16, parameter :: y = 1.1Q+4932
+print*,x,y
+end
