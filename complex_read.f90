49ea231a3a6b034dad97371248958f351766cfd7
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2024-01-19 12:04:25 -0500

    read complex variable as a whole or as two real numbers

--- complex_read.f90
+++ complex_read.f90
@@ -2,9 +2,9 @@ implicit none ! GitHub FortranTip complex_read.f90
 character (len=20) :: s
 complex :: z
 s = "(1.0, 4.5)"
-read (s,*) z
+read (s,*) z ! read entire complex variable
 print*,z ! (1.0,4.5)
-s = "1.0, 4.5"
-read (s,*) z%re, z%im
+s = "1.0, 4.5"        ! not parenthesized
+read (s,*) z%re, z%im ! read real and imag. parts separately
 print*,z ! (1.0,4.5)
 end
