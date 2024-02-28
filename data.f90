739e3fcc9e16c49733ee10cd658aec22e06b2abb
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2024-02-28 06:41:10 -0500

    Variables set by a DATA statement in a procedure have the SAVE attribute

--- data.f90
+++ data.f90
@@ -2,11 +2,12 @@ module m ! GitHub FortranTip data.f90
 implicit none
 contains
 function factorial(n) result(ifac)
+! buggy code
 integer, intent(in) :: n
 integer :: ifac
 integer :: i, j
-data i/1/
-do j=1,n
+data i/1/ ! write "i=1" instead
+do j=2,n
    i = i*j
 end do
 ifac = i
@@ -16,5 +17,5 @@ end module m
 program main
 use m
 implicit none
-print*,factorial(3), factorial(4)
+print*,factorial(3), factorial(4) ! gives 6 144
 end program main
